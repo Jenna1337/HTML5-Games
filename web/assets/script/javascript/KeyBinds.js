34e4ac995f53ddc03a2a915287e4aabe406406ae
Jenna Sloan <jenna3715@gmail.com>
2019-03-17 00:36:19 -0500

    Added keydown/keyup callbacks to KeyBinds.js

--- web/assets/script/javascript/KeyBinds.js
+++ web/assets/script/javascript/KeyBinds.js
@@ -1,36 +1,42 @@
 var keybinds=[];
 var keybindspreventdefault = true;
+window.KEY_WILDCARD = "AnyKey";
 var addKeyBind = (function(){
-	var addKeyBind = function(key, callback, id=keybinds.length){
-		keybinds[id]={"key":key,"callback":callback};
+	var addKeyBind = function(key, callback, ondown=()=>{}, onup=()=>{}, id=keybinds.length){
+		keybinds[id]={"key":key,"callback":callback,"ondown":ondown,"onup":onup,"active":false};
 		//keybinds.push({"key":key,"callback":callback,"id":id});
 	}
 	const checkkey = function(k, key){
-		return k==key.toLowerCase();
+		return key==KEY_WILDCARD || k==key.toLowerCase();
+	}
+	const setIsActive = function(bind, isdownevent, key){
+		//if(bind.active != isdownevent)
+			bind[isdownevent ? "ondown" : "onup"](key);
+		bind.active=isdownevent;
 	}
 	const iteratebinds = function(e, isdownevent=e.type=='keydown'){
+		if(keybindspreventdefault)
+			e.preventDefault()
 		var k = e.key.toLowerCase();
 		for(var bind of keybinds){
 			if(Array.isArray(bind.key))
 				for(var b of bind.key){
 					if(checkkey(k,b)){
-						bind.active=isdownevent;
+						setIsActive(bind,isdownevent,k);
 						break;
 					}
 				}
 			else if(checkkey(k,bind.key))
-				bind.active=isdownevent;
+				setIsActive(bind,isdownevent,k);
 		}
-		if(keybindspreventdefault)
-			e.preventDefault()
 	}
 	const keyDownHandler = function(e){
 		if(!document.hasFocus)
 			return;
-		iteratebinds(e);
+		iteratebinds(e,true);
 	}
 	const keyUpHandler = function(e){
-		iteratebinds(e);
+		iteratebinds(e, false);
 	}
 	window.addEventListener("keydown", keyDownHandler, false);
 	window.addEventListener("keyup", keyUpHandler, false);
